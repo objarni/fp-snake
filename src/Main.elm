1ca60fbd1d30fb6cd12c09eba3291c1b63276189
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-03-04 21:59:49 +0100

    visualize head of worm

--- src/Main.elm
+++ src/Main.elm
@@ -3,15 +3,21 @@ module Main exposing (..)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
-type alias Model = Int
+type alias Coord = {x: Int, y: Int}
+
+type alias Model = {
+   count: Int,
+   head: Coord
+ }
 
 init : Model
 init =
-  0
+  { count=0, head={x=10, y=10} }
 
 type Msg = Increment | Decrement
 
@@ -19,16 +25,27 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 2
+      {model | count=model.count + 2}
 
     Decrement ->
-      model - 1
+      {model | count=model.count - 1}
 
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+  let 
+    drawHead {x, y} = div [
+      style "background-color" "orange",
+      style "position" "relative",
+      style "width" "10px",
+      style "height" "10px",
+      style "left" ((String.fromInt (10 * x) ++ "px")),
+      style "top" ((String.fromInt (10 * y)) ++ "px")
+      ] [text " "]
+  in
+    div [ style "height" "500px"
+        , style "width" "500px"
+        , style "background-color" "black"
+        --, style "position" "relative"
+        ]
+      [ drawHead model.head ]
