cccf637835b970d2cb935a25e9f615b3609ecab4
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-03-05 16:31:34 +0100

    visualize 2 body parts

--- src/Main.elm
+++ src/Main.elm
@@ -10,14 +10,18 @@ main =
 
 type alias Coord = {x: Int, y: Int}
 
-type alias Model = {
-   count: Int,
-   head: Coord
- }
+type alias Model = 
+  { count: Int
+  , head: Coord
+  , bodyParts: List Coord
+  }
 
 init : Model
 init =
-  { count=0, head={x=10, y=10} }
+  { count=0
+  , head={x=10, y=10}
+  , bodyParts=[{x=8, y=10}, {x=9, y=10}]
+  }
 
 type Msg = Increment | Decrement
 
@@ -31,21 +35,24 @@ update msg model =
       {model | count=model.count - 1}
 
 
+viewCell color {x, y} = div [
+  style "background-color" color,
+  style "position" "absolute",
+  style "width" "10px",
+  style "height" "10px",
+  style "left" ((String.fromInt (10 * x) ++ "px")),
+  style "top" ((String.fromInt (10 * y)) ++ "px")
+  ] [text " "]
+
+
+drawHead coord = viewCell "orange" coord
+
+
 view : Model -> Html Msg
 view model =
-  let 
-    drawHead {x, y} = div [
-      style "background-color" "orange",
-      style "position" "relative",
-      style "width" "10px",
-      style "height" "10px",
-      style "left" ((String.fromInt (10 * x) ++ "px")),
-      style "top" ((String.fromInt (10 * y)) ++ "px")
-      ] [text " "]
-  in
-    div [ style "height" "500px"
-        , style "width" "500px"
-        , style "background-color" "black"
-        --, style "position" "relative"
-        ]
-      [ drawHead model.head ]
+  div [ style "height" "500px"
+      , style "width" "500px"
+      , style "background-color" "black"
+      , style "position" "relative"
+      ]
+    ([ drawHead model.head] ++ List.map (viewCell "white") model.bodyParts )
