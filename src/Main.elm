b4c2899bce1944e7f65eab741a34ea060f5635f1
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-03-05 23:28:27 +0100

    Escape moves head

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,23 @@
 module Main exposing (..)
 
 import Browser
+import Browser.Dom as Dom
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (style, tabindex, id)
+import Keyboard exposing (Key(..))
+import Keyboard.Events as Keyboard
+import Task
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = subscriptions
+    }
+
+type alias Flags = {}
 
 type alias Coord = {x: Int, y: Int}
 
@@ -16,23 +27,34 @@ type alias Model =
   , bodyParts: List Coord
   }
 
-init : Model
-init =
-  { count=0
+type Msg = Increment | Decrement | NoOp
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+init : Flags -> (Model, Cmd Msg)
+init flags = ({ count=0
   , head={x=10, y=10}
   , bodyParts=[{x=8, y=10}, {x=9, y=10}]
-  }
+  }, Dom.focus "app-div" |> Task.attempt (always NoOp) )
 
-type Msg = Increment | Decrement
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Increment ->
-      {model | count=model.count + 2}
+      ({ model
+      | count=model.count + 2
+      , head={x=model.head.x+1,y=model.head.y}
+      }, Cmd.none)
 
     Decrement ->
-      {model | count=model.count - 1}
+      ({model | count=model.count - 1}, Cmd.none)
+
+    NoOp -> (model, Cmd.none)
 
 
 viewCell color {x, y} = div [
@@ -54,5 +76,8 @@ view model =
       , style "width" "500px"
       , style "background-color" "black"
       , style "position" "relative"
+      , tabindex 0
+      , id "app-div"
+      , Keyboard.on Keyboard.Keydown [(Escape, Increment)]
       ]
     ([ drawHead model.head] ++ List.map (viewCell "white") model.bodyParts )
